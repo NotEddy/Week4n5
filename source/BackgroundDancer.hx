f10568193607df3b0885ab532a42204fb5fac968
NotEddy <eddysdumbedits@gmail.com>
2022-06-07 16:39:09 -0700

    custom background dancer

--- source/BackgroundDancer.hx
+++ source/BackgroundDancer.hx
@@ -9,7 +9,17 @@ class BackgroundDancer extends FlxSprite
 	{
 		super(x, y);
 
-		frames = Paths.getSparrowAtlas("limo/limoDancer");
+		switch (curStage)
+		{
+			case 'limoVitas': 
+				frames = Paths.getSparrowAtlas("limo/limoDancer");
+		    case 'limoThank': 
+				frames = Paths.getSparrowAtlas("limoThank/limoDancer");
+			case 'limoJp': 
+				frames = Paths.getSparrowAtlas("limoJp/limoDancer");
+			default:
+			    frames = Paths.getSparrowAtlas("limoJp/limo");
+		}
 		animation.addByIndices('danceLeft', 'bg dancer sketch PINK', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		animation.addByIndices('danceRight', 'bg dancer sketch PINK', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		animation.play('danceLeft');
