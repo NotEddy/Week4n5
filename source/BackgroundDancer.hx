2204362a40aa0fc65952fa91265d42a29aa501af
NotEddy <eddysdumbedits@gmail.com>
2022-06-07 17:28:53 -0700

    omg

--- source/BackgroundDancer.hx
+++ source/BackgroundDancer.hx
@@ -2,31 +2,21 @@ package;
 
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
-import Song.SwagSong;
 
 class BackgroundDancer extends FlxSprite
 {
 	
 	public static var curStage:String = '';
 	public static var SONG:SwagSong = null;
+
+    var songName:String = Paths.formatToSongPath(SONG.song);
+    curStage = PlayState.SONG.stage;	
 	
 	public function new(x:Float, y:Float)
 	{
 		super(x, y);
-				
-        var songName:String = Paths.formatToSongPath(SONG.song);
-		curStage = PlayState.SONG.stage;
-		switch (curStage)
-		{
-			case 'limoVitas': 
-				frames = Paths.getSparrowAtlas("limo/limoDancer");
-		    case 'limoThank': 
-				frames = Paths.getSparrowAtlas("limoThank/limoDancer");
-			case 'limoJp': 
-				frames = Paths.getSparrowAtlas("limoJp/limoDancer");
-			default:
-			    frames = Paths.getSparrowAtlas("limoJp/limo");
-		}
+
+		frames = Paths.(curStage + "("/limoDancer")");
 		animation.addByIndices('danceLeft', 'bg dancer sketch PINK', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		animation.addByIndices('danceRight', 'bg dancer sketch PINK', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		animation.play('danceLeft');
