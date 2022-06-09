af978a94f0413b01841909c4ae1c1bb170a1e10f
NotEddy <eddysdumbedits@gmail.com>
2022-06-08 18:24:20 -0700

    revert

--- source/BackgroundDancer.hx
+++ source/BackgroundDancer.hx
@@ -9,17 +9,17 @@ class BackgroundDancer extends FlxSprite
 	public function new(x:Float, y:Float)
 	{
 		
-		var static:String = N;
+		var stat:string = '';
 		
 		if(!ClientPrefs.lowQuality) {
-			static = N;
+			stat = '';
 		} else {
-			static = Static;
+			stat = Static;
 		}
 		
 		super(x, y);
 
-		frames = Paths.getSparrowAtlas(PlayState.curStage + ("/limoDancer") + Staic);
+		frames = Paths.getSparrowAtlas(PlayState.curStage + ("/limoDancer") + stat);
 		animation.addByIndices('danceLeft', 'bg dancer sketch PINK', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		animation.addByIndices('danceRight', 'bg dancer sketch PINK', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		animation.play('danceLeft');
